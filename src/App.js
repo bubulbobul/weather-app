dfcd00b2537c96429f4b4510225af2e0bd44894f
windam <windam@live>
2019-07-22 16:02:53 +0530

    add css class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 
 import Title from "./components/Title";
 import Form from "./components/Form";
@@ -45,16 +44,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Title />
-        <Form getWeather={this.getWeather} />
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+        <div className='wrapper'>
+          <div className='main'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-xs-5 title-container'>
+                  <Title />
+                </div>
+                <div className='col-xs-7 form-container'>
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,23 +1,57 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Weather(props) {
-  const { temperature, city, country, humidity, description, error } = props;
-  return (
-    <div>
-      {city && country && (
-        <p>
-          Location: {city}, {country}
-        </p>
-      )}
-      {temperature && <p>Temperature: {temperature}</p>}
-      {humidity && <p>Humidity: {humidity}</p>}
-      {description && <p>Conditions: {description}</p>}
-      {error && <p>{error}</p>}
-    </div>
-  );
-}
+// function Weather(props) {
+//   const { temperature, city, country, humidity, description, error } = props;
+//   return (
+//     <div className='weather__info'>
+//       {city && country && (
+//         <p>
+//           Location: {city}, {country}
+//         </p>
+//       )}
+//       {temperature && <p>Temperature: {temperature}</p>}
+//       {humidity && <p>Humidity: {humidity}</p>}
+//       {description && <p>Conditions: {description}</p>}
+//       {error && <p>{error}</p>}
+//     </div>
+//   );
+// }
 
-Weather.propTypes = {};
+const Weather = props => (
+  <div className='weather__info'>
+    {props.city && props.country && (
+      <p className='weather__key'>
+        Locatiom:
+        <span className='weather__value'>
+          {" "}
+          {props.city}, {props.country}
+        </span>
+      </p>
+    )}
+    {props.temperature && (
+      <p className='weather__key'>
+        Temperature:
+        <span className='weather__value'> {props.temperature} </span>
+      </p>
+    )}
+    {props.humidity && (
+      <p className='weather__key'>
+        Humidity:
+        <span className='weather__value'> {props.humidity} </span>
+      </p>
+    )}
+    {props.description && (
+      <p className='weather__key'>
+        Conditions:
+        <span className='weather__value'> {props.description}</span>
+      </p>
+    )}
+    {props.error && (
+      <p className='weather__error'>
+        <span>{props.error} </span>
+      </p>
+    )}
+  </div>
+);
 
 export default Weather;
