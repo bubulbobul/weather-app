8c9366dd4d30a47ac7edfd6f76a7d8d4c354aca1
bubulbobul <windam@live.fr>
2019-08-19 19:54:52 +0530

    Updates

---
+++ precache-manifest.0ccb5f6b233f6114ca4727dc6c343a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d18ff716b35a1c6e999986f2c518c1",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "23e405c46b555bf515b8",
+    "url": "/weather-app/static/css/main.d53229c1.chunk.css"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/weather-app/static/js/2.e597a5a4.chunk.js"
+  },
+  {
+    "revision": "23e405c46b555bf515b8",
+    "url": "/weather-app/static/js/main.e7d1ac7b.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
